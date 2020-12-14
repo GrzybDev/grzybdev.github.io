7f676ab6ea7bdb24d51629c07a012a50caf5c5ee
Marek Grzyb <grzybdev@gmail.com>
2020-12-14 15:38:09 +0100

    Added new sections to site

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import Particles from "react-particles-js";
 import particlesConfig from "./config/particles.json";
 import "./App.css";
 import Landing from "./sections/Landing";
+import Experience from "./sections/Experience";
+import PerfectScrollbar from 'react-perfect-scrollbar'
+import Projects from "./sections/Projects";
+import Contact from "./sections/Contact";
 
 function App() {
   return (
   <main>
     <Particles className="bgParticles" height="100vh" width="100vw" params={particlesConfig} />
 
-    <div id="PageContent">
+    <PerfectScrollbar id="PageContent">
       <Landing />
-    </div>
+      <Experience />
+      <Projects />
+      <Contact />
+    </PerfectScrollbar>
   </main>);
 }
 
