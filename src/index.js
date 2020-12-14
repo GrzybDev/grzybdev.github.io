f8ccadf60b8a701a8ecf9937264d82f7f498d427
Marek Grzyb <grzybdev@gmail.com>
2020-12-14 15:37:02 +0100

    Updated and added stylesheet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./index.css";
+import 'react-perfect-scrollbar/dist/css/styles.css';
 
 ReactDOM.render(
     <React.StrictMode>
