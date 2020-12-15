72af574c35f785c12efd7538ef5a0313df44f190
Marek Grzyb <grzybdev@gmail.com>
2020-12-15 12:09:17 +0100

    Removed perfect scrollbar and added new components to site

--- src/App.js
+++ src/App.js
@@ -3,21 +3,27 @@ import particlesConfig from "./config/particles.json";
 import "./App.css";
 import Landing from "./sections/Landing";
 import Experience from "./sections/Experience";
-import PerfectScrollbar from 'react-perfect-scrollbar'
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
+import Navbar from "./Navbar";
+import BackToTop from "./BackToTop";
+import Preloader from "./Preloader";
 
 function App() {
   return (
   <main>
     <Particles className="bgParticles" height="100vh" width="100vw" params={particlesConfig} />
 
-    <PerfectScrollbar id="PageContent">
+    <div id="PageContent">
       <Landing />
       <Experience />
       <Projects />
       <Contact />
-    </PerfectScrollbar>
+    </div>
+
+    <Navbar />
+    <BackToTop />
+    <Preloader />
   </main>);
 }
 
