24be3feef91852f1e4aeb1709001651d00406c58
Marek Grzyb <grzybdev@gmail.com>
2020-12-16 14:52:35 +0100

    Landing section is now dynamic!

--- src/sections/Landing.js
+++ src/sections/Landing.js
@@ -1,4 +1,3 @@
-import { faLevelUpAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import ReactTyped from 'react-typed';
 import "./Landing.css";
@@ -7,27 +6,51 @@ const { Component } = require("react");
 
 class Landing extends Component {
 
+    state = {
+        name: "",
+        selfieUrl: "",
+        selfieAlt: "",
+        signText: "",
+        signIcon: "",
+        proffesions: [],
+        about: ""
+    }
+
     getName() {
-        return "Marek Grzyb".split('').map((letter, index) => (
+        return this.state.name.split('').map((letter, index) => (
             <span className="animatedLetter" key={index}>{letter}</span>
         ));
     }
 
+    applyData(data) {
+        this.setState({
+            name: data.personal.name,
+            selfieUrl: data.personal.selfieUrl,
+            selfieAlt: data.site.texts.selfie_alternative,
+            signIcon: data.site.texts.arrow_up_icon,
+            signText: data.site.texts.selfie_caption,
+            proffesions: data.personal.proffesions,
+            about: data.personal.about_me
+        });
+    }
+
     render() {
+        if (this.state.name === "") return null;
+
         return (
             <section id="landing">
                 <div className="left">
-                    <img className="selfie" src="#TODO" alt="My selfie" />
+                    <img className="selfie" src={this.state.selfieUrl} alt={this.state.selfieAlt} />
                     <div className="sign">
-                        <p>It's me!&nbsp;&nbsp;&nbsp;</p>
-                        <FontAwesomeIcon icon={faLevelUpAlt} />
+                        <p dangerouslySetInnerHTML={{ __html: this.state.signText }} />
+                        <FontAwesomeIcon icon={this.state.signIcon} />
                     </div>
                 </div>
 
                 <div className="right">
                     <h1 className="title">{this.getName()}</h1>
-                    <ReactTyped className="proffesions" strings={["Lorem ipsum", "Ipsum sum"]} typeSpeed={40} backSpeed={50} backDelay={2000} cursorChar="_" loop/>
-                    <p className="desc" dangerouslySetInnerHTML={{ __html: "Lorem ipsum" }} />
+                    <ReactTyped className="proffesions" strings={this.state.proffesions} typeSpeed={40} backSpeed={50} backDelay={2000} cursorChar="_" loop />
+                    <p className="desc" dangerouslySetInnerHTML={{ __html: this.state.about }} />
                 </div>
             </section>
         )
