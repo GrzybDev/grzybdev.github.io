bd97a9b15abfe59f07431025b21b60a63e5778a7
Marek Grzyb <grzybdev@gmail.com>
2020-12-16 14:50:58 +0100

    Rewritten App component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,73 @@
 import Particles from "react-particles-js";
-import particlesConfig from "./config/particles.json";
 import "./App.css";
 import Landing from "./sections/Landing";
 import Experience from "./sections/Experience";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
-import Navbar from "./Navbar";
-import BackToTop from "./BackToTop";
-import Preloader from "./Preloader";
+import Navbar from "./elements/Navbar";
+import BackToTop from "./elements/BackToTop";
+import Preloader from "./elements/Preloader";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import React from "react";
 
-function App() {
-  return (
-  <main>
-    <Particles className="bgParticles" height="100vh" width="100vw" params={particlesConfig} />
+class App extends React.Component {
 
-    <div id="PageContent">
-      <Landing />
-      <Experience />
-      <Projects />
-      <Contact />
-    </div>
+  state = {
+    particlesConfig: {}
+  }
 
-    <Navbar />
-    <BackToTop />
-    <Preloader />
-  </main>);
+  constructor() {
+    super();
+
+    library.add(fas);
+    library.add(fab);
+
+    this.navbarRef = React.createRef();
+    this.backToTopRef = React.createRef();
+    
+    this.landingRef = React.createRef();
+    this.experienceRef = React.createRef();
+  }
+
+  loadData(type, data) {
+    switch (type) {
+      case "common": {
+        this.setState({ particlesConfig: data.particles.background });
+        
+        this.navbarRef.current.applyData(data);
+        this.backToTopRef.current.applyData(data);
+
+        this.landingRef.current.applyData(data);
+        this.experienceRef.current.applyData(data, 1);
+
+        window.scrollTo(0, 0);
+        break;
+      }
+      default:
+        break;
+    }
+  }
+
+  render() {
+    return (
+      <main>
+        <Particles className="bgParticles" height="100vh" width="100vw" params={this.state.particlesConfig} />
+
+        <div id="PageContent">
+          <Landing ref={this.landingRef} />
+          <Experience ref={this.experienceRef} />
+          <Projects />
+          <Contact />
+        </div>
+
+        <Navbar ref={this.navbarRef} />
+        <BackToTop ref={this.backToTopRef} />
+        <Preloader handler={this.loadData.bind(this)} />
+    </main>
+    )
+  }
 }
 
 export default App;
