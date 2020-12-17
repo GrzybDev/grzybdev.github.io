72449063facf7547a0ceaca698b4f6120081d527
Marek Grzyb <grzybdev@gmail.com>
2020-12-17 15:02:19 +0100

    Projects section is now dynamic

--- src/sections/Projects.js
+++ src/sections/Projects.js
@@ -1,58 +1,121 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Component } from "react";
+import { API_SERVER } from "../config";
 import "./Projects.css"
 
 class Projects extends Component {
+
+    state = {
+        sectionName: "",
+        sectionDesc: "",
+        sources: [],
+        projects: {}
+    }
+
+    applyData(data, id) {
+        this.setState({
+            sectionName: data.common.sections[id].name,
+            sectionDesc: data.common.sections[id].description,
+            sources: data.projects.sources
+        });
+
+        const newProjects = this.state.projects;
+
+        this.state.sources.forEach((entry, index) => {
+            switch (entry.method) {
+                case "api":
+                    fetch(`${API_SERVER}/portfolio/projects`)
+                        .then(response => (response.json()))
+                        .then(data => {
+                            this.props.projectHandler(data.length);
+                            if (data.length === 0) return;
+
+                            newProjects[index] = data;
+                            this.setState({ projects: newProjects });
+                        });
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
+
+    renderBadges(badgesData) {
+        return badgesData.map((entry, index) => (
+            <div className="badge" style={{ backgroundColor: entry.color }} key={index}>{entry.text}</div>
+        ));
+    }
+
+    renderHighlights(highlightsData) {
+        return highlightsData.map((entry, index) => (
+            <li key={index}>{entry}</li>
+        ));
+    }
+
+    renderProjects(id) {
+        if (!this.state.projects[id]) return (<h1 className="noProjects" dangerouslySetInnerHTML={{ __html: `No projects found!<br/>Please check again later` }} />);
+
+        return this.state.projects[id].map((entry, index) => (
+            <div className="entry">
+                <a href={`/project/${entry.id}`}>
+                    <div className="tile" key={index}>
+                        <div className="bg" style={{ backgroundImage: `url('${entry.tile}')` }} />
+                        <div className="tileTitle">
+                            {entry.name}
+                        </div>
+                    </div>
+                </a>
+
+                <div className="badges">
+                    {this.renderBadges(entry.badges)}
+                </div>
+
+                <div className="shortDescription">
+                    {entry.shortDescription}
+                </div>
+
+                <br />
+
+                <div className="highlights">
+                    <p>Highlights:</p>
+                    <ul>
+                        {this.renderHighlights(entry.highlights)}
+                    </ul>
+                </div>
+            </div>
+        ));
+    }
+
     render() {
+        if (this.state.sectionName === "") return null;
+
+        const subsections = this.state.sources.map((entry, index) => (
+            <div className="subsection" key={index}>
+                <div className="title">
+                    <FontAwesomeIcon icon={entry.icon} />&nbsp;{entry.name}&nbsp;<FontAwesomeIcon icon={entry.icon} />
+                </div>
+
+                <br />
+
+                <div className="list">
+                    {this.renderProjects(index)}
+                </div>
+            </div>
+        ));
+
         return (
             <section id="projects">
                 <div className="sectionTitle">
-                    <h1>Projects</h1>
+                    <h1>{this.state.sectionName}</h1>
                 </div>
 
                 <div className="sectionDesc">
-                    <p>Click on the tiles to learn more about each project.</p>
+                    <p dangerouslySetInnerHTML={{ __html: this.state.sectionDesc }} />
                 </div>
 
                 <hr />
 
-                <div className="subsection">
-                    <div className="title">
-                        <FontAwesomeIcon icon={faUser} />&nbsp;Personal Projects&nbsp;<FontAwesomeIcon icon={faUser} />
-                    </div>
-
-                    <br />
-
-                    <div className="list">
-                        <div className="entry">
-                            <div className="tile">
-                                <div className="bg" style={{ backgroundImage: "url('')" }} />
-                                <div className="tileTitle">
-                                    Test Project
-                                </div>
-                            </div>
-
-                            <div className="badges">
-                                <div className="badge" style={{ backgroundColor: 'red' }}>Test</div>
-                                <div className="badge" style={{ backgroundColor: 'red' }}>Test</div>
-                                <div className="badge" style={{ backgroundColor: 'red' }}>Test</div>
-                                <div className="badge" style={{ backgroundColor: 'red' }}>Test</div>
-                            </div>
-
-                            <div className="shortDescription">
-                            </div>
-
-                            <br />
-                            <div className="highlights">
-                                Highlights:
-                                <div className="list">
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {subsections}
             </section>
         )
     }
