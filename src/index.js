3cbc2efe81a656cc202dabf041380ac6ca4ea493
Marek Grzyb <grzybdev@gmail.com>
2020-12-21 13:59:50 +0100

    Updated index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import App from './pages/App';
 import "./index.css";
+import ProjectSite from './pages/Project';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Switch>
+                <Route path="/project/:id">
+                    <ProjectSite />
+                </Route>
+
+                <Route path="*">
+                    <App />
+                </Route>
+            </Switch>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
