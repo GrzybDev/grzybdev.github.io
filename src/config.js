f423bdeffb2ce2e9302ba8287d15be6b52a277fb
Marek Grzyb <grzybdev@gmail.com>
2020-12-26 12:19:25 +0100

    Added firebaseConfig

--- src/config.js
+++ src/config.js
@@ -1,1 +1,11 @@
 export const API_SERVER = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? "http://localhost:8080" : "https://grzybdev.herokuapp.com";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyB_nyC6aAwRL1e-U0qQNgbPipJWzctnCuc",
+    authDomain: "grzybdev-portfolio.firebaseapp.com",
+    projectId: "grzybdev-portfolio",
+    storageBucket: "grzybdev-portfolio.appspot.com",
+    messagingSenderId: "1031569808119",
+    appId: "1:1031569808119:web:4c93fd67491f41927f51f2",
+    measurementId: "G-STQL0LFPN2"
+};
